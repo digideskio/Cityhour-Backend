85c5df9e663e6142b0b6e36c7e04082afc42d69a
Denis Bukashk0zzz <Bukashk0zzz@me.com>
2013-11-06 11:37:16 +0200

    open map

--- helpers/map.php
+++ helpers/map.php
@@ -1,16 +1,16 @@
 <?php
 
- $data = '
-    {
-      "private_key": "42530ec9ae353de65dbc386b8d9bd9449ac99b845231c239d9c6a",
-      "debug": true,
-      "offset": 10800,
-      "lat": "50.4362640380859400",
-      "lng": "30.5156993865966800"
-    }
- ';
+// $data = '
+//    {
+//      "private_key": "42530ec9ae353de65dbc386b8d9bd9449ac99b845231c239d9c6a",
+//      "debug": true,
+//      "offset": 10800,
+//      "lat": "50.4362640380859400",
+//      "lng": "30.5156993865966800"
+//    }
+// ';
 
-//$data = file_get_contents("php://input");
+$data = file_get_contents("php://input");
 $data = json_decode($data,true);
 $debug = (isset($data["debug"]) && $data["debug"]) ? true:false;
 
