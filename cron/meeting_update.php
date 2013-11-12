23291275233fa3536fa9258d9f20b210fe3ffbd3
Denis Bukashk0zzz <Bukashk0zzz@me.com>
2013-11-12 16:45:05 +0200

    meet update fix

--- cron/meeting_update.php
+++ cron/meeting_update.php
@@ -27,6 +27,7 @@ if (mysql_num_rows($result) != 0) {
     while ($row = mysql_fetch_assoc($result)) {
         $data['place'] = $row['place'];
         $text = Application_Model_Texts::notification($data)[11];
+        $text = mysql_real_escape_string($text);
         $id = $row["id"];
         $user_id = $row["user_id"];
         mysql_query("insert into notifications (`from`, `to`, `type`, `action`, `template`, `item`, `text`) values ('0','$user_id','11','1','4','$id','$text')");
@@ -53,6 +54,7 @@ if (mysql_num_rows($result) != 0) {
     while ($row = mysql_fetch_assoc($result)) {
         $data['place'] = $row['place'];
         $text = Application_Model_Texts::notification($data)[10];
+        $text = mysql_real_escape_string($text);
         $id = $row["id"];
         $user_id = $row["user_id"];
         $data = array(
@@ -63,7 +65,7 @@ if (mysql_num_rows($result) != 0) {
         );
         $data = json_encode($data);
         mysql_query("insert into push_messages (`user_id`, `type`, `alert`, `data`) values ('$user_id','8','$text2','$data')");
-        mysql_query("insert into notifications (`from`, `to`, `type`, `action`, `template`, `item`, `text`) values ('0','$user_id','10','7','3','$id','$text')");
+        mysql_query("insert into notifications (`from`, `to`, `type`, `action`, `template`, `item`, `text`) values ('0','$user_id','10','7','0','$id','$text')");
     }
 }
 mysql_free_result($result);
