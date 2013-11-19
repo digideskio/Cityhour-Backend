f7c72e7bec4ca50ba71325a316c18e5ceb9b6e67
Denis Bukashk0zzz <Bukashk0zzz@me.com>
2013-11-19 15:13:34 +0200

    #CH-182 fix

--- cron/push_send.php
+++ cron/push_send.php
@@ -3,7 +3,7 @@ require "db.class.php";
 $db = new DBcron();
 
 while (true) {
-    $all = $db->query("select group_concat(id) as ids from push_messages where `status` = 0",false,true);
+    $all = $db->query("select group_concat(id) as ids from push_messages where `status` = 0",true,false);
     if (isset($all['ids'])) {
         echo exec('/bin/bash push_send.sh '.$all['ids']);
     }
