1c78ea176dd10137606a553e05a86422fea68eeb
Denis Bukashk0zzz <Bukashk0zzz@me.com>
2013-11-19 10:00:02 +0200

    test map

--- helpers/map.php
+++ helpers/map.php
@@ -1,16 +1,16 @@
 <?php
 
-// $data = '
-//    {
-//      "private_key": "42530ec9ae353de65dbc386b8d9bd9449ac99b845231c239d9c6a",
-//      "debug": true,
-//      "offset": 10800,
-//      "lat": "50.4362640380859400",
-//      "lng": "30.5156993865966800"
-//    }
-// ';
+ $data = '
+    {
+      "private_key": "c3077d5cd3efe0797fb516b3cb216b3d55242f425221f6a71b83c",
+      "debug": true,
+      "offset": 7200,
+      "lat": "50.4362640380859400",
+      "lng": "30.5156993865966800"
+    }
+ ';
 
-$data = file_get_contents("php://input");
+//$data = file_get_contents("php://input");
 $data = json_decode($data,true);
 $debug = (isset($data["debug"]) && $data["debug"]) ? true:false;
 
